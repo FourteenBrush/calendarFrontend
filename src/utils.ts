a288ac67e5bb86a515950cf26f9b2d8db1889686
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2024-12-16 22:31:18 +0100

    Full year support in getCalendarDays()

--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,62 @@
 export const DAYS_PER_WEEK = 7
 export const WEEK_DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] as const
 
+const MONTHS_LONG = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+] as const
+
 export const daysInMonth = (today: Date) => {
   return new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
 }
 
-export type Day = {
-  date: Date,
-  dayOfWeek: typeof WEEK_DAYS[number],
-  isCurrentMonth: boolean,
+export const dateToMonthAndYear = (date: Date): string => {
+  const month = MONTHS_LONG[date.getMonth()]
+  return `${month} ${date.getFullYear()}`
 }
 
-export const getCalendarDays = (year: number, month: number): Day[] => {
-  const firstDayOfMonth = new Date(year, month - 1, 1)
-  const daysPrepended = (firstDayOfMonth.getDay() + 6) % DAYS_PER_WEEK
+export const getDayOfMonth = (): number => new Date().getDate()
 
-  const dayCount = new Date(year, month, 0).getDate()
+export type Day = Readonly<{
+  date: Date,
+  weekDay: WeekDay,
+  isCurrentMonth: boolean,
+}>
+
+// DO NOT REORDER
+export enum WeekDay {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+const isLeapYear = (year: number): boolean => {
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)
+}
+
+export const getCalendarDays = (year: number, monthIdx: number): Day[] => {
+  const firstDayOfYear = new Date(year, monthIdx, 1)
+  const daysPrepended = (firstDayOfYear.getDay() + 6) % DAYS_PER_WEEK
+  const dayCount = daysPrepended + (isLeapYear(year) ? 366 : 365)
+  const daysAppended = (DAYS_PER_WEEK - dayCount % DAYS_PER_WEEK) % DAYS_PER_WEEK
+
   return Array.from(
-    { length: daysPrepended + dayCount },
-    (_, i) => ({
-      date: new Date(year, month - 1, i + 1 - daysPrepended),
-      dayOfWeek: WEEK_DAYS[i % DAYS_PER_WEEK],
-      isCurrentMonth: i >= daysPrepended,
-    }),
+    { length: dayCount + daysAppended },
+    (_, i) => {
+      const date = new Date(year, monthIdx, i + 1 - daysPrepended)
+      const weekDay = (i % DAYS_PER_WEEK) as WeekDay
+      const isCurrentMonth = monthIdx === date.getMonth() && year === date.getFullYear()
+      return { date, weekDay, isCurrentMonth }
+    }
   )
 }
 
-//for (let month = 1; month <= 12; month++) {
-//  console.table(getCalendarDays(2024, month).map(day => ({
-//    data: day.date.toLocaleDateString(),
-//    isCurrentMonth: day.isCurrentMonth,
-//    dayOfWeek: day.dayOfWeek,
-//  })))
-//}
+//console.table(getCalendarDays(2024, 12).map(day => ({
+//  date: day.date.toLocaleDateString(),
+//  isCurrentMonth: day.isCurrentMonth,
+//  weekDay: day.weekDay,
+//})))
