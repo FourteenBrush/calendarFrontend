6c9514b3c3bd4d26b8eae33bf2875f39061a643b
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2024-12-16 22:34:04 +0100

    Platform specific theme constants

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,8 @@
-import { TextStyle, ViewStyle } from "react-native"
+import { Platform, TextStyle, ViewStyle } from "react-native"
+
+export const BORDER_RADIUS = Platform.select({ web: 9, default: 1 })
+export const APP_CONTAINER_PADDING = Platform.select({ web: 12, default: 0 })
+export const MIN_CELL_HEIGHT = Platform.select({ web: 45, default: 22 })
 
 // NOTE: properties are defined as ViewStyle (or something similar) for the ease of writing
 // `style={[theme.something, theme.somethingElse]}`
