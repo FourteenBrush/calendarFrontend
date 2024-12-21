971d4a87227aa2f888776d23a49625e42bf5522d
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2024-12-21 23:08:15 +0100

    Move api_call.ts to api_calls.ts

--- src/api_call.ts
+++
@@ -1,5 +0,0 @@
-const BASE_URL = process.env.API_BASE_URL
-if (BASE_URL == null) {
-  throw new Error("env variable API_BASE_URL must be set")
-}
-
--- src/api_calls.ts
+++ src/api_calls.ts
@@ -1,5 +1,12 @@
-const BASE_URL = process.env.API_BASE_URL
+import axios from "axios"
+
+const BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL
 if (BASE_URL == null) {
   throw new Error("env variable API_BASE_URL must be set")
 }
 
+export const login = async (email: string, password: string) => {
+  const response = await axios.post(`${BASE_URL}/auth/login`, {
+    email, password,
+  })
+}
